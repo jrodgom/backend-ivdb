38e83cddad1120010677108785c288890be4e7a3
jrodgom <jesusdaniel.rodriguezgomez@iesvalleinclan.es>
2025-11-06 22:44:00 +0100

    filtrado de juegos

--- ivdb/game/api/views.py
+++ ivdb/game/api/views.py
@@ -1,12 +1,13 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from game.models import Game
 from game.api.serializers import GameSerializer
 
-
 class GameViewSet(viewsets.ModelViewSet):
-    queryset = Game.objects.all()
+    queryset = Game.objects.all().order_by('id')
     serializer_class = GameSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'genre', 'platform']
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
